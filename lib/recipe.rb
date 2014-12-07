654e667eb57a93ef1ddc45cba7c1bcb2a87684bd
Paul Weidinger <paul.weidinger@gmail.com>
2014-12-07 00:45:19 -0500

    adding recipe class implementation

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,5 +1,37 @@
 require_relative 'ingredient'
 
 class Recipe
+  attr_accessor :ingredients, :title, :instructions
+
+  def initialize(title, ingredients, instructions = "")
+    @title = title
+    @ingredients = Set.new
+    ingredients.each {|i| @ingredients << i}
+    @instructions = instructions
+  end
+
+  def getHealth
+  end
+
+  def wrap(s, width=78)
+    s.gsub(/(.{1,#{width}})(\s+|\Z)/, "\\1\n")
+  end
+
+  def to_s
+    str = ""
+    max = 0
+    @ingredients.each do |i|
+      max = [i.name.length, max].max
+    end
+
+    @ingredients.each do |i|
+      str << sprintf("%s%3.2f %-4s\n", i.name.ljust(max+4, '.'), i.qty, i.unit)
+    end
+
+    str << wrap(@instructions, max+14)
+
+    str = "#{@title}".center(max+11) << "\n\n" << str
+    return str
+  end
 
 end
